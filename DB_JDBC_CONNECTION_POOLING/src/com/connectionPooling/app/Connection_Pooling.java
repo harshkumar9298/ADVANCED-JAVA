f3945025bc9d882f19038dd3bb76cd5430937686
CWS <CWS@DESKTOP-QTNCLAJ>
2023-05-19 23:42:27 +0530

    updated;

--- DB_JDBC_CONNECTION_POOLING/src/com/connectionPooling/app/Connection_Pooling.java
+++ DB_JDBC_CONNECTION_POOLING/src/com/connectionPooling/app/Connection_Pooling.java
@@ -13,9 +13,10 @@ public class Connection_Pooling {
 			 this.uName=uName;
 			 this.pWord=pWord;
 		}
-
+		//Instance reference variable(Tightly Coupled reference)
 		public Vector<Connection> v = new Vector<Connection>();
 		 
+		//Instance method
 		 public void createConnections() {
 			 try {
 				 while(v.size()<5) {
@@ -28,11 +29,22 @@ public class Connection_Pooling {
 					 if(v.size()==5) {
 					 System.out.println("Pool is full...."); 
 					 }
-			 }
+			 }//end of method
 			 catch(Exception e) 
 		 		{
 				 e.printStackTrace();
 		 		} 
 		 	}//end of method
-
+		 	
+		 public synchronized Connection useConnection() {
+			 Connection con = v.elementAt(0);
+			 //Taking the element from index 0
+			 v.removeElementAt(0);//Element deleted from Vector
+			 return con;
+			 }//end of method
+		 
+		public synchronized void returnConnection(Connection con) {
+			 v.addElement(con);//Adding the Connection back to Vector
+			 System.out.println("Connection added back to pool...");
+			 }//end of method
 		}
--- DB_JDBC_CONNECTION_POOLING/src/com/connectionPooling/app/Connection_Pooling_Main.java
+++ DB_JDBC_CONNECTION_POOLING/src/com/connectionPooling/app/Connection_Pooling_Main.java
@@ -1,12 +1,63 @@
 package com.connectionPooling.app;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
 public class Connection_Pooling_Main {
 
 	public static void main(String[] args) {
 		Connection_Pooling cp =
 			new Connection_Pooling("jdbc:oracle:thin:@localhost:1521:ORCL","system","harsh");
-		 cp.createConnections();
+		 cp.createConnections();//Method call
+		
+		 System.out.println("*****User-1*****"); 
+		 Connection cn1 = cp.useConnection();
+		 System.out.println("Con at user-1 : "+cn1);
+		
+		 System.out.println("Size of pool :"+cp.v.size());
+		 try {
+		 PreparedStatement ps1 =
+		                cn1.prepareStatement("Select * from Product52"); 
+		 ResultSet rs1 = ps1.executeQuery();
+		 while(rs1.next()) {
+		 System.out.println(rs1.getString(1)+"\t"+ rs1.getString(2)+"\t"+rs1.getFloat(3)+ "\t"+rs1.getInt(4));
+		 }//end of loop
+		 }catch(Exception e) 
+		 {
+			 e.printStackTrace();
+		 } 
+		 System.out.println("****User-2****");
+		 Connection cn2 = cp.useConnection();
+		 System.out.println("Con at user-2 : "+cn2); 
+		 System.out.println("Size of pool :"+cp.v.size());
+		 try {
+			 PreparedStatement ps2 = cn2.prepareStatement ("Select * from Emp52");
+			 ResultSet rs2 = ps2.executeQuery();
+			 while(rs2.next()) {
+					 System.out.println(rs2.getString(1)+ 
+					 "\t"+rs2.getString(2)+
+					 "\t"+rs2.getString(3)+
+					 "\t"+rs2.getInt(4));
+			 	}//end of loop
+		 }
+		 catch(Exception e) 
+		 	{
+			 	e.printStackTrace();
+		 	} 
+		 System.out.println("*****User-1****");
+		 cp.returnConnection(cn1);
+		 System.out.println("Size of pool :"+cp.v.size());
+		 System.out.println("*****User-2****");
+		 cp.returnConnection(cn2);
+		 System.out.println("Size of pool :"+cp.v.size());
+		 System.out.println("-----Display Connections----");
+		 cp.v.forEach((k)->
+		 {
+					 System.out.println(k);
+		 	});
+		}
 
-	}
+	
 
 }
